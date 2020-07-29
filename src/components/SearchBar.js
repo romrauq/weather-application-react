ef26e5261eff4c50bb576eee9b14bc097e36f665
John Quarmor <johnequarmor@gmail.com>
2020-07-29 07:45:44 +0000

    Renamed component className & edited css styles.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,9 @@ import React from "react";
 
 const SearchBar = () => {
 	return (
-		<div className="searchbar">
-			<input type="text" name="" className="search-bar" />
-			<i
-				className="fas fa-search float-right pt-1 pl-1 search-icon"
-				type="button"
-			></i>
+		<div className="searchbar-container">
+			<input type="text" />
+			<i className="fas fa-search search-icon" type="button"></i>
 		</div>
 	);
 };
