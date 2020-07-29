d53761de18f1ceb45ee8857bd65a385af01c8153
John Quarmor <johnequarmor@gmail.com>
2020-07-29 12:07:32 +0000

    Modified styles for WeatherTabs component

--- src/components/WeatherTabs.js
+++ src/components/WeatherTabs.js
@@ -2,33 +2,31 @@ import React from "react";
 
 const WeatherTabs = () => {
 	return (
-		<div className="d3">
-			<div className="d3-1">
-				<div className="weather-panel">
-					<p>12:00</p>
-					<i className="fas fa-cloud icons"></i>
-					<p>24 C</p>
-				</div>
-				<div className="weather-panel">
-					<p>1:00</p>
-					<i className="fas fa-cloud-showers-heavy icons"></i>
-					<p>24 C</p>
-				</div>
-				<div className="weather-panel">
-					<p>2:00</p>
-					<i className="fas fa-cloud-sun icons"></i>
-					<p>24 C</p>
-				</div>
-				<div className="weather-panel">
-					<p>3:00</p>
-					<i className="fas fa-sun icons"></i>
-					<p>24 C</p>
-				</div>
-				<div className="weather-panel">
-					<p>4:00</p>
-					<i className="fas fa-cloud icons"></i>
-					<p>24 C</p>
-				</div>
+		<div className="weathertab-component">
+			<div className="weather-tab">
+				<p>12:00</p>
+				<i className="fas fa-cloud tab-icons"></i>
+				<p>24 C</p>
+			</div>
+			<div className="weather-tab">
+				<p>1:00</p>
+				<i className="fas fa-cloud-showers-heavy tab-icons"></i>
+				<p>24 C</p>
+			</div>
+			<div className="weather-tab">
+				<p>2:00</p>
+				<i className="fas fa-cloud-sun tab-icons"></i>
+				<p>24 C</p>
+			</div>
+			<div className="weather-tab">
+				<p>3:00</p>
+				<i className="fas fa-sun tab-icons"></i>
+				<p>24 C</p>
+			</div>
+			<div className="weather-tab">
+				<p>4:00</p>
+				<i className="fas fa-cloud tab-icons"></i>
+				<p>24 C</p>
 			</div>
 		</div>
 	);
