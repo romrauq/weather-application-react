8cbc4b741c1f7f68895519df85e95bab8af86dfb
John Quarmor <johnequarmor@gmail.com>
2020-08-08 20:15:04 +0000

    Added placeholder text to SearchBar component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,12 @@ import React from "react";
 const SearchBar = () => {
 	return (
 		<div className="searchbar-component">
-			<input type="text" />
-			<i className="fas fa-search search-icon" type="button"></i>
+			<input type="text" placeholder="Search a Location Here" />
+			<i
+				id="search-button"
+				className="fas fa-search search-icon"
+				type="button"
+			></i>
 		</div>
 	);
 };
