28197c66ba4a5c1b86c7eeb6e92b776a503b9815
John Quarmor <johnequarmor@gmail.com>
2020-08-11 07:50:39 +0000

    Added props to AdditionalInfo component.

--- src/components/AdditionalInfo.js
+++ src/components/AdditionalInfo.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-const AdditionalInfo = () => {
+const AdditionalInfo = (props) => {
 	return (
 		<div className="additional-info-component">
-			<p className="additional-info-heading">Additional information:</p>
+			<h3 className="additional-info-heading">Additional information:</h3>
 			<div className="additional-info-sections">
 				<div className="additional-info-section">
 					<p className="additional-info-title">Precipitation</p>
-					<p className="additional-info-status">3%</p>
+					<p className="additional-info-status">{props.precipitation}</p>
 				</div>
 				<div className="additional-info-section">
 					<p className="additional-info-title">Humidity</p>
-					<p className="additional-info-status">74%</p>
+					<p className="additional-info-status">{props.humidity}%</p>
 				</div>
 				<div className="additional-info-section">
 					<p className="additional-info-title">Wind Speed</p>
-					<p className="additional-info-status">10Km/h</p>
+					<p className="additional-info-status">{props.windspeed}Km/h</p>
 				</div>
 			</div>
 		</div>
