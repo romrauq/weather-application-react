52b99ca7de6d666b7b1b6de33e605eead5e683da
John Quarmor <johnequarmor@gmail.com>
2020-08-12 09:07:32 +0000

    Added "ampm" prop toWeatherTabIcon component.

--- src/components/WeatherTabIcon.js
+++ src/components/WeatherTabIcon.js
@@ -3,7 +3,10 @@ import React from "react";
 const WeatherTabIcon = (props) => {
 	return (
 		<div className="weather-tab">
-			<p>{props.time}</p>
+			<p>
+				{props.time}
+				{props.ampm}
+			</p>
 			<i className={props.icon}></i>
 			<p>{props.temp}</p>
 		</div>
