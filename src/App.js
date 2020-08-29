d77524530fced99cb067d791b3eb486e0d52946e
John Quarmor <johnequarmor@gmail.com>
2020-08-29 19:56:24 +0000

    Created the methods 'handleChange' & 'searchAction', passed them both through the rendered component searchBar as props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Graph from "./components/Graph";
 
 class App extends Component {
 	state = {
-		searchText: "Accra",
-		country: "Ghana",
-		city: "Greater Accra",
+		searchLocation: "",
+		country: "Ghana (default)",
+		city: "Greater Accra (default)",
 		date: new Date().toDateString(),
 		img_alt: "Location Image Details",
 		precipitation: 3,
@@ -19,22 +19,30 @@ class App extends Component {
 		windspeed: 10,
 	};
 
-	// Fetch APIs to run upon main application page loads:
+	handleChange = (e) => {
+		this.setState({ searchLocation: e.target.value });
+	};
+
+	searchAction = () => {
+		console.log(this.state.searchLocation);
+	};
+
+	// Component lifecycle to run fetch API(s) upon initial page render:
 	componentDidMount() {
-		// 1st fetch() passes a string value from state.searchText to retrieve location Key:
+		// 1st fetch() request passes searched text value from the state to retrieve the location Key from HTTP request's response data:
 		fetch(
-			`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.searchText}`
+			`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.searchLocation}`
 		)
-			.then(function (res) {
-				return res.json();
+			.then(function (response) {
+				return response.json();
 			})
 			.then(function (data) {
-				// 2nd fetch has location Key passed through it retrieved from the previous fetch response to retrieve 12 Hour weather data:
+				// 2nd fetch request uses the location Key to retrieve 12 Hour weather data of its respective location:
 				fetch(
 					`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${data[0].Key}?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&language=en-us&details=true&metric=false`
 				)
-					.then(function (res) {
-						return res.json();
+					.then(function (response) {
+						return response.json();
 					})
 					.then(function (data) {
 						console.log(data);
@@ -45,7 +53,10 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<SearchBar />
+				<SearchBar
+					handleChange={this.handleChange}
+					searchText={this.searchAction}
+				/>
 
 				<LocationInfo
 					country={this.state.country}
