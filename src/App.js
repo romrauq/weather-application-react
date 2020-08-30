2e0474e9dcbd65c0caf3d174adadb0b52d1da97d
John Quarmor <johnequarmor@gmail.com>
2020-08-30 19:54:45 +0000

    Placed fetch API within a conditional function to be run when the search icon is clicked. Passed props value into location condition in rendered LocationInfo component.

--- src/App.js
+++ src/App.js
@@ -10,45 +10,47 @@ import Graph from "./components/Graph";
 class App extends Component {
 	state = {
 		searchLocation: "",
-		country: "Ghana (default)",
-		city: "Greater Accra (default)",
+		country: "[Country]",
+		city: "[City]",
 		date: new Date().toDateString(),
-		img_alt: "Location Image Details",
+		img_alt: "[Location Image Name]",
+		locationCondition: "Cloudy",
 		precipitation: 3,
 		humidity: 74,
 		windspeed: 10,
 	};
 
+	// Assigning typed text value to state.searchLocation:
 	handleChange = (e) => {
 		this.setState({ searchLocation: e.target.value });
 	};
 
+	// Actions to run when search button is clicked:
 	searchAction = () => {
-		console.log(this.state.searchLocation);
+		if (this.state.searchLocation !== "") {
+			// 1st fetch() request passes searched text value from the state to retrieve the location Key from HTTP request's response data:
+			fetch(
+				`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.searchLocation}`
+			)
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (data) {
+					// 2nd fetch request retrieve 12 Hour weather data of its respective location using location Key as argument for location query:
+					fetch(
+						`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${data[0].Key}?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&language=en-us&details=true&metric=false`
+					)
+						.then(function (response) {
+							return response.json();
+						})
+						.then(function (data) {
+							console.log(data);
+						});
+				});
+		} else {
+			return null;
+		}
 	};
-
-	// Component lifecycle to run fetch API(s) upon initial page render:
-	componentDidMount() {
-		// 1st fetch() request passes searched text value from the state to retrieve the location Key from HTTP request's response data:
-		fetch(
-			`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.searchLocation}`
-		)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (data) {
-				// 2nd fetch request uses the location Key to retrieve 12 Hour weather data of its respective location:
-				fetch(
-					`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${data[0].Key}?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&language=en-us&details=true&metric=false`
-				)
-					.then(function (response) {
-						return response.json();
-					})
-					.then(function (data) {
-						console.log(data);
-					});
-			});
-	}
 
 	render() {
 		return (
@@ -63,6 +65,7 @@ class App extends Component {
 					city={this.state.city}
 					date={this.state.date}
 					alt={this.state.img_alt}
+					location_condition={this.state.locationCondition}
 				/>
 
 				<WeatherTabs />
