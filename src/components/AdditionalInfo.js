9d6744a08a3cb2de4a5ac2606bcc7e23a7f03670
John Quarmor <johnequarmor@gmail.com>
2020-08-30 19:51:44 +0000

    Commented out heading text, added className to middle child element in component and gave it borders.

--- src/components/AdditionalInfo.js
+++ src/components/AdditionalInfo.js
@@ -3,13 +3,13 @@ import React from "react";
 const AdditionalInfo = (props) => {
 	return (
 		<div className="additional-info-component">
-			<h3 className="additional-info-heading">Additional information:</h3>
+			{/* <h3 className="additional-info-heading">Additional Information:</h3> */}
 			<div className="additional-info-sections">
 				<div className="additional-info-section">
 					<p className="additional-info-title">Precipitation</p>
 					<p className="additional-info-status">{props.precipitation}%</p>
 				</div>
-				<div className="additional-info-section">
+				<div className="additional-info-section middle-info-section">
 					<p className="additional-info-title">Humidity</p>
 					<p className="additional-info-status">{props.humidity}%</p>
 				</div>
