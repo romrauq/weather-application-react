4dcc3c409ae51ca1f683eb9ff88f357e70054dd6
John Quarmor <johnequarmor@gmail.com>
2020-08-31 20:43:47 +0000

    Edited temporary state values, added temporary icon object to state, renamed some state items, started scripting setState scripts within the data section of the 1st fetch() request function.

--- src/App.js
+++ src/App.js
@@ -9,26 +9,34 @@ import Graph from "./components/Graph";
 
 class App extends Component {
 	state = {
-		searchLocation: "",
-		country: "[Country]",
-		city: "[City]",
+		search: "",
+		country: "{Country}",
+		city: "{City}",
 		date: new Date().toDateString(),
-		img_alt: "[Location Image Name]",
-		locationCondition: "Cloudy",
+		img_alt: "{Location Image Name}",
+		location_icon: "",
+		location_condition: "Cloudy",
 		precipitation: 3,
 		humidity: 74,
 		windspeed: 10,
+		// Temporary object below:
+		icons: {
+			cloudy: "fas fa-cloud tab-icons",
+			rain: "fas fa-cloud-showers-heavy tab-icons",
+			cloud_sun: "fas fa-cloud-sun tab-icons",
+			sunny: "fas fa-sun tab-icons",
+		},
 	};
 
-	// Assigning typed text value to state.searchLocation:
+	// Assigning typed text value to this.state.searchLocation:
 	handleChange = (e) => {
-		this.setState({ searchLocation: e.target.value });
+		this.setState({ search: e.target.value });
 	};
 
-	// Actions to run when search button is clicked:
+	// Actions to be executed when search button is clicked:
 	searchAction = () => {
-		if (this.state.searchLocation !== "") {
-			// 1st fetch() request passes searched text value from the state to retrieve the location Key from HTTP request's response data:
+		if (this.state.search !== "") {
+			// 1st fetch() request passes searched text value from the state to retrieve the searched location Key from HTTP request's response data:
 			fetch(
 				`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.searchLocation}`
 			)
@@ -36,19 +44,26 @@ class App extends Component {
 					return response.json();
 				})
 				.then(function (data) {
-					// 2nd fetch request retrieve 12 Hour weather data of its respective location using location Key as argument for location query:
+					console.log(data);
+					// this.setState({ country: data[0].Country.LocalizedName });
+					this.setState({
+						country: this.data[0].Country.LocalizedName,
+						city: this.data[0].LocalizedName,
+					});
+					// 2nd fetch request retrieve 12 Hour weather:
 					fetch(
-						`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${data[0].Key}?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&language=en-us&details=true&metric=false`
+						`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${data[0].Key}?apikey=lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&language=en-us&details=true&metric=true`
 					)
 						.then(function (response) {
 							return response.json();
 						})
-						.then(function (data) {
-							console.log(data);
+						.then(function (weatherData) {
+							console.log(weatherData);
 						});
 				});
 		} else {
-			return null;
+			// return null;
+			alert("Please enter a location in the text box...");
 		}
 	};
 
@@ -65,7 +80,9 @@ class App extends Component {
 					city={this.state.city}
 					date={this.state.date}
 					alt={this.state.img_alt}
-					location_condition={this.state.locationCondition}
+					// location_icon={this.state.location_icon}
+					location_icon={this.state.icons.cloud_sun}
+					location_condition={this.state.location_condition}
 				/>
 
 				<WeatherTabs />
