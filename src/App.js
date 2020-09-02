bd1d989efb6cf9a95b4874a3407f4e02a2ba5b3b
John Quarmor <johnequarmor@gmail.com>
2020-09-02 08:18:57 +0000

    Renamed state property name and edited comments.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Graph from "./components/Graph";
 
 class App extends Component {
 	state = {
-		search: "",
+		search_query: "",
 		country: "{Country}",
 		city: "{City}",
 		date: new Date().toDateString(),
@@ -19,7 +19,7 @@ class App extends Component {
 		precipitation: 3,
 		humidity: 74,
 		windspeed: 10,
-		// Temporary object below:
+		// TEMPORARY "icons" object below:
 		icons: {
 			cloudy: "fas fa-cloud tab-icons",
 			rain: "fas fa-cloud-showers-heavy tab-icons",
@@ -28,17 +28,18 @@ class App extends Component {
 		},
 	};
 
-	// Assigning typed text value to this.state.searchLocation:
+	// Function to assign typed text value to this.state.search_query:
 	handleChange = (e) => {
-		this.setState({ search: e.target.value });
+		this.setState({ search_query: e.target.value });
 	};
 
-	// Actions to be executed when search button is clicked:
+	// Function(s) to be executed when search button is clicked:
 	searchAction = () => {
-		if (this.state.search !== "") {
-			// 1st fetch() request passes searched text value from the state to retrieve the searched location Key from HTTP request's response data:
+		if (this.state.search_query !== "") {
+			// 1st fetch() request passes searched text value from the state.search_query
+			// to fetch data containing the a location Key from the HTTP request's response data:
 			fetch(
-				`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.searchLocation}`
+				`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&q=${this.state.search_query}`
 			)
 				.then(function (response) {
 					return response.json();
@@ -50,7 +51,7 @@ class App extends Component {
 						country: this.data[0].Country.LocalizedName,
 						city: this.data[0].LocalizedName,
 					});
-					// 2nd fetch request retrieve 12 Hour weather:
+					// 2nd fetch request retrieves 12 Hour weather:
 					fetch(
 						`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${data[0].Key}?apikey=lulVbGjWKvDKKR7fVOza26BTcRrc8NIW&language=en-us&details=true&metric=true`
 					)
@@ -63,7 +64,7 @@ class App extends Component {
 				});
 		} else {
 			// return null;
-			alert("Please enter a location in the text box...");
+			alert("Enter your location in the text area before searching");
 		}
 	};
 
@@ -81,7 +82,7 @@ class App extends Component {
 					date={this.state.date}
 					alt={this.state.img_alt}
 					// location_icon={this.state.location_icon}
-					location_icon={this.state.icons.cloud_sun}
+					location_icon={this.state.icons.cloud_sun} // Replace with above script when able to use icon data from state.
 					location_condition={this.state.location_condition}
 				/>
 
