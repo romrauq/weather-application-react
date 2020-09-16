d1ee1a9a4ca92a4456eef3bf471f9abeaf30fb76
John Quarmor <johnequarmor@gmail.com>
2020-09-16 15:33:55 +0000

    1. Removed icon object variable. 2. Edited props values of WeatherTabs props (temp, icon).

--- src/components/WeatherTabs.js
+++ src/components/WeatherTabs.js
@@ -1,13 +1,6 @@
 import React from "react";
 import WeatherTabIcon from "./WeatherTabIcon";
 
-const icon = {
-	cloudy: "fas fa-cloud tab-icons",
-	rain: "fas fa-cloud-showers-heavy tab-icons",
-	cloud_sun: "fas fa-cloud-sun tab-icons",
-	sunny: "fas fa-sun tab-icons",
-};
-
 // Creating a variable which contains the current hour value:
 let hour = new Date().getHours();
 
@@ -39,32 +32,32 @@ const WeatherTabs = (props) => {
 			<WeatherTabIcon
 				time={setTime(hour)}
 				ampm={setAMPM(hour)}
-				icon={icon.cloudy} // Replace with {props.weather_icon_0}
-				temp={props.weather_temp_0}
+				icon={props.icon_0} // Replace with {props.weather_icon_0}
+				temp={props.temp_0}
 			/>
 			<WeatherTabIcon
 				time={setTime(hour + 1)}
 				ampm={setAMPM(hour + 1)}
-				icon={icon.cloud_sun} // Replace with {props.weather_icon_1}
-				temp={props.weather_temp_1}
+				icon={props.icon_1} // Replace with {props.weather_icon_1}
+				temp={props.temp_1}
 			/>
 			<WeatherTabIcon
 				time={setTime(hour + 2)}
 				ampm={setAMPM(hour + 2)}
-				icon={icon.rain} // Replace with {props.weather_icon_2}
-				temp={props.weather_temp_2}
+				icon={props.icon_2} // Replace with {props.weather_icon_2}
+				temp={props.temp_2}
 			/>
 			<WeatherTabIcon
 				time={setTime(hour + 3)}
 				ampm={setAMPM(hour + 3)}
-				icon={icon.cloud_sun} // Replace with {props.weather_icon_3}
-				temp={props.weather_temp_3}
+				icon={props.icon_3} // Replace with {props.weather_icon_3}
+				temp={props.temp_3}
 			/>
 			<WeatherTabIcon
 				time={setTime(hour + 4)}
 				ampm={setAMPM(hour + 4)}
-				icon={icon.sunny} // Replace with {props.weather_icon_4}
-				temp={props.weather_temp_4}
+				icon={props.icon_4} // Replace with {props.weather_icon_4}
+				temp={props.temp_4}
 			/>
 		</div>
 	);
