d882cf0758fc5c73027b54968c4e839ec6c1924f
John Quarmor <johnequarmor@gmail.com>
2020-09-24 18:22:21 +0000

    Imported charts.js and implemented in in the DOM.

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Chart from "chart.js";
 
 const Graph = () => {
+	useEffect(() => {
+		const ctx = document.getElementById("myChart").getContext("2d");
+		new Chart(ctx, {
+			// The type of chart we want to create
+			type: "line",
+
+			// The data for our dataset
+			data: {
+				labels: [
+					"January",
+					"February",
+					"March",
+					"April",
+					"May",
+					"June",
+					"July",
+				],
+				datasets: [
+					{
+						label: "Tempetarure dataset",
+						backgroundColor: "rgb(50, 205, 50)",
+						borderColor: "rgb(50, 205, 50)",
+						data: [0, 10, 5, 2, 20, 30, 45],
+					},
+				],
+			},
+
+			// Configuration options go here
+			options: {},
+		});
+	});
 	return (
 		<div className="graph-component">
-			<p className="graph-content">[Graph Displays Here]</p>
+			<canvas id="myChart" style={{ width: "100%", height: "100%" }}></canvas>
 		</div>
 	);
 };
