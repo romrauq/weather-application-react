334ce823c558ff35d033f0c11b4805b1692bafb7
John Quarmor <johnequarmor@gmail.com>
2020-09-25 11:08:44 +0000

    1. Added various time functions to component. 2. Passed time returned values to chart.

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,30 +1,65 @@
 import React, { useEffect } from "react";
 import Chart from "chart.js";
 
+// Creating a variable which contains the current hour value:
+let hour = new Date().getHours();
+
+// Function to set time to 12 hour format:
+function setTime(val) {
+	if (val === 24) {
+		return 12;
+	} else if (val > 12) {
+		return val % 12;
+	} else {
+		return val;
+	}
+}
+
+//Condition to suffix "AM" or "PM" to hour value:
+function setAMPM(val) {
+	if (val >= 24) {
+		return "am";
+	} else if (val >= 12) {
+		return "pm";
+	} else {
+		return "am";
+	}
+}
+
 const Graph = () => {
 	useEffect(() => {
 		const ctx = document.getElementById("myChart").getContext("2d");
 		new Chart(ctx, {
-			// The type of chart we want to create
-			type: "line",
+			// Defining chart type:
+			type: "bar",
 
-			// The data for our dataset
+			// The data for our dataset:
 			data: {
 				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
+					`${setTime(hour)} ${setAMPM(hour)}`,
+					`${setTime(hour + 1)} ${setAMPM(hour + 1)}`,
+					`${setTime(hour + 2)} ${setAMPM(hour + 2)}`,
+					`${setTime(hour + 3)} ${setAMPM(hour + 3)}`,
+					`${setTime(hour + 4)} ${setAMPM(hour + 4)}`,
 				],
 				datasets: [
 					{
-						label: "Tempetarure dataset",
-						backgroundColor: "rgb(50, 205, 50)",
-						borderColor: "rgb(50, 205, 50)",
-						data: [0, 10, 5, 2, 20, 30, 45],
+						label: "Precipitation",
+						backgroundColor: "skyblue",
+						borderColor: "rgb(50, 205, 150)",
+						data: [5, 10, 3, 0, 14],
+					},
+					{
+						label: "Humidity",
+						backgroundColor: "rgb(50, 190, 150)",
+						borderColor: "rgb(150, 250, 50)",
+						data: [24, 23, 25, 24, 21],
+					},
+					{
+						label: "Wind Speed",
+						backgroundColor: "#777",
+						borderColor: "rgb(250, 0, 0)",
+						data: [24, 23, 25, 24, 21],
 					},
 				],
 			},
