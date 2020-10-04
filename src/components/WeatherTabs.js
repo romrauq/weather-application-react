f6bc9b90c1fc71c5e5d492e3ffcdfa199d983b17
John Quarmor <johnequarmor@gmail.com>
2020-10-04 12:45:23 +0000

    Edited rendered component name(s)

--- src/components/WeatherTabs.js
+++ src/components/WeatherTabs.js
@@ -1,34 +1,34 @@
 import React from "react";
-import WeatherTabIcon from "./WeatherTabIcon";
+import WeatherTab from "./WeatherTab";
 
 const WeatherTabs = (props) => {
 	return (
 		<div className="weathertabs-component">
-			<WeatherTabIcon
+			<WeatherTab
 				hour={props.setTime(props.hour)}
 				ampm={props.setAMPM(props.hour)}
 				icon={props.icon_0}
 				temp={props.temp_0}
 			/>
-			<WeatherTabIcon
+			<WeatherTab
 				hour={props.setTime(props.hour + 1)}
 				ampm={props.setAMPM(props.hour + 1)}
 				icon={props.icon_1}
 				temp={props.temp_1}
 			/>
-			<WeatherTabIcon
+			<WeatherTab
 				hour={props.setTime(props.hour + 2)}
 				ampm={props.setAMPM(props.hour + 2)}
 				icon={props.icon_2}
 				temp={props.temp_2}
 			/>
-			<WeatherTabIcon
+			<WeatherTab
 				hour={props.setTime(props.hour + 3)}
 				ampm={props.setAMPM(props.hour + 3)}
 				icon={props.icon_3}
 				temp={props.temp_3}
 			/>
-			<WeatherTabIcon
+			<WeatherTab
 				hour={props.setTime(props.hour + 4)}
 				ampm={props.setAMPM(props.hour + 4)}
 				icon={props.icon_4}
