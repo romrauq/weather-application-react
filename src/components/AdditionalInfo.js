5290ec3e34977617f74cb4fab0a25d2a24b37bb7
John Quarmor <johnequarmor@gmail.com>
2020-10-14 12:13:56 +0000

    Created style object variables. Passed object variables as style properties within markup elements.

--- src/components/AdditionalInfo.js
+++ src/components/AdditionalInfo.js
@@ -1,21 +1,48 @@
 import React from "react";
+const component_styles = {
+	width: "98%",
+	padding: "5px",
+	margin: "0px auto 5px",
+	backgroundColor: "pink",
+	borderRadius: "5px",
+	cursor: "default",
+};
+const section_container = {
+	display: "flex",
+	justifyContent: "space-around",
+};
+const title_styles = {
+	fontSize: "1rem",
+	fontWeight: "bolder",
+	textAlign: "center",
+	margin: "0",
+};
+const center_section = {
+	padding: "0 5px",
+	borderLeft: "1px solid #555",
+	borderRight: "1px solid #555",
+};
+const info = {
+	fontSize: "0.8rem",
+	textAlign: "center",
+	margin: "0",
+};
 
 const AdditionalInfo = (props) => {
 	return (
-		<div className="additional-info-component">
-			{/* <h3 className="additional-info-heading">Additional Information</h3> */}
-			<div className="additional-info-sections">
-				<div className="additional-info-section">
-					<p className="additional-info-title">Precipitation</p>
-					<p className="additional-info-status">{props.precipitation}%</p>
+		<div style={component_styles}>
+			<div style={section_container}>
+				<div>
+					<p style={title_styles}>Precipitation</p>
+					<p style={info}>{props.precipitation}%</p>
 				</div>
-				<div className="additional-info-section middle-info-section">
-					<p className="additional-info-title">Humidity</p>
-					<p className="additional-info-status">{props.humidity}%</p>
+				<div style={center_section}>
+					<p style={title_styles}>Humidity</p>
+					<p style={info}>{props.humidity}%</p>
 				</div>
-				<div className="additional-info-section">
-					<p className="additional-info-title">Wind Speed</p>
-					<p className="additional-info-status">{props.windspeed}Km/h</p>
+				<div>
+					<p style={title_styles}>Wind Speed</p>
+					<p style={info}>{props.windspeed}Km/h</p>
 				</div>
 			</div>
 		</div>
