658748497ade2026be84ae8595b23e4039c941a6
John Quarmor <johnequarmor@gmail.com>
2020-10-14 11:40:53 +0000

    1. Changed component into class component. 2. Imported react-autocomplete module. 3. Began setup for autocomplete element/markup.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import Autocomplete from "react-autocomplete";
 
-const SearchBar = (props) => {
-	return (
-		<div className="searchbar-component">
-			<input
-				type="text"
-				autoFocus={true}
-				placeholder="Enter Your City Name"
-				onChange={props.handleChange}
-			/>
+class SearchBar extends Component {
+	state = {
+		query: "",
+	};
+	render() {
+		return (
+			<div className="searchbar-component">
+				<div className="autocomplete-wrapper">
+					<Autocomplete
+						value={this.state.query}
+						// autoItems={this.state.city_options}
+						// getItemValue={(cityList) => data.localizedName}
+						shouldItemRender={this.renderMovieTitle}
+						renderMenu={(item) => <div className="dropdown">{item}</div>}
+						renderItem={(item, isHighlighted) => (
+							<div className={`item ${isHighlighted ? "selected-item" : ""}`}>
+								{item.title}
+							</div>
+						)}
+						onChange={(event, value) => this.setState({ value })}
+						onSelect={(value) => this.setState({ value })}
+					/>
+				</div>
+				<i
+					id="search-button"
+					className="fas fa-search search-icon"
+					type="button"
+					onClick={this.props.searchAction}
+				></i>
+			</div>
+		);
+	}
 
-			<i
-				id="search-button"
-				className="fas fa-search search-icon"
-				type="button"
-				onClick={props.searchAction}
-			></i>
-		</div>
-	);
-};
+	renderMovieTitle = (state, val) => {
+		return state.title.toLowerCase().indexOf(val.toLowerCase()) !== -1;
+	};
+}
 
 export default SearchBar;
